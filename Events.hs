f89ed3efcf3acfc77f9b7437e0f853dee9bf699c
Dave Compton <davecompton7@gmail.com>
2016-06-06 01:15:46 +0000

    trimmed down, cleaned up

--- Events.hs
+++ Events.hs
@@ -1,27 +1,31 @@
-{-# LANGUAGE RecursiveDo #-}
+import Reflex.Dom 
 import Data.Map (fromList)
-
-import Reflex.Dom (elAttr', elDynAttrNS', constDyn, dynText, mainWidget, wrapDomEvent, onEventName, EventName(Mousemove),performEvent_, holdDyn, _el_element, (=:))
-
 import GHCJS.DOM.EventM (mouseOffsetXY) 
-import Data.Monoid ((<>))
-
--- | Namespace needed for svg elements.
-svgNamespace = Just "http://www.w3.org/2000/svg"
 
 main = mainWidget $ do 
-    let style = "border:3px solid black; margin:1em; margin-top:2em; margin-bottom:2em; padding:4em; display:inline-block"
-    rec 
-        (el, ev) <- elDynAttrNS' svgNamespace "svg" 
-                        (constDyn $  "viewBox" =: ("0 0 1 1 ")
-                                  <> "width" =: "300"
-                                  <> "height" =: "250"
-                                  <> "style" =: style)
+    let attrs =   constDyn $ 
+                      fromList 
+                          [ ("width" , "500")
+                          , ("height" , "250")
+                          , ("style" , "border:solid; margin:8em")
+                          ]
 
-                        $ return ()
-        (x,_) <- (elAttr' "div" (fromList [("style",style)]) $ dynText t)
-        e <- wrapDomEvent (_el_element el) (onEventName Mousemove) mouseOffsetXY
-        performEvent_ $ return () <$ e
-        t <- holdDyn "" (show <$> e)
-    return ()
+    (elm, _) <-   elDynAttrNS' 
+                      (Just "http://www.w3.org/2000/svg")
+                      "svg" 
+                      attrs 
+                      (return ())
+
+    mouseEvent <- wrapDomEvent 
+                      (_el_element elm) 
+                      (onEventName Mousemove) 
+                      mouseOffsetXY
+
+    performEvent_ $ return () <$ mouseEvent
+
+    let mouseXYToString (x,y) = "X = " ++ show x ++ ";Y = " ++ show y
+
+    t <- holdDyn "" (mouseXYToString <$> mouseEvent)
+
+    el "div" $ dynText t
 
